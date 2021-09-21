e9150b93b4749a545995fee90927c28e4cbd9e41
musasyncs <rubato.cw@gmail.com>
2021-09-21 13:40:20 +0800

    Initial Commit

---
+++ Journal App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journal App
+//
+//  Created by Ewen on 2021/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
